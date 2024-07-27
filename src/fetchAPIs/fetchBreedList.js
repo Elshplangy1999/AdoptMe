fcef3d0bd96b1472328e82e394a2ce35f64108b0
Emad Elshplangy <93099274+Elshplangy1999@users.noreply.github.com>
2024-07-27 20:34:06 +0300

    Delete src directory

--- src/fetchAPIs/fetchBreedList.js
+++
@@ -1,9 +0,0 @@
-export async function fetchBreedList({ queryKey }) {
-  const animal = queryKey[1];
-  if (!animal) return [];
-  const req = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
-  if (!req.ok) {
-    throw new Error(`details/${animal} fetch not ok`);
-  }
-  return req.json();
-}
--- src/fetchAPIs/fetchPetDetails.js
+++
@@ -1,8 +0,0 @@
-export default async function fetchPetDetails({ queryKey }) {
-  const id = queryKey[1];
-  const req = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
-  if (!req.ok) {
-    throw new Error(`details/${id} fetch not ok`);
-  }
-  return req.json();
-}
